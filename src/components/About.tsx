99251d53cc3546a18313bb536f19c7f586169dca
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2023-10-26 21:07:20 +0600

    add pictures

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,6 +14,7 @@ const About = () => {
             width={100}
             height={100}
             src={me}
+            alt="a young women"
             className="rounded-full object-cover"
           />
           <h1 className="text-3xl font-semibold">Ramilia Imankulova</h1>
