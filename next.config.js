3675efecc91b079e22f3dcc456ff54ddb93f11ae
Ramilia Imankulova <ramiliaimankulova@Ramilias-MacBook-Pro.local>
2023-10-27 00:12:36 +0600

    add links

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  // If your custom layout is in the "src/app" directory, specify it here:
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
